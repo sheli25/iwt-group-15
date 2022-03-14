ea28022e5b41e6d4d368d18da48137fddc87bfea
jannatun akter sheli <101557363+sheli25@users.noreply.github.com>
2022-03-14 21:46:05 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php include('partials/menu.php');?>
+<div class="main-content">
+    <div class="wrapper">
+  <h1>DASHBOARD</h1>
+  <div class="col-4">
+      <h1>5</h1>
+      <br>
+        categories
+  </div>
+  <div class="col-4">
+      <h1>5</h1>
+      <br>
+        categories
+  </div>
+  <div class="col-4">
+      <h1>5</h1>
+      <br>
+        categories
+  </div>
+  <div class="col-4">
+      <h1>5</h1>
+      <br>
+        categories
+  </div>
+  <div class="clearfix"></div>
+</div>
+</div>
+  </div>
+
+<?php include('partials/footer.php')?>
